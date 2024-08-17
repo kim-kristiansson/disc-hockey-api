f8776a4f103bfd1d7f93be5f27df8abb8dd54032
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-17 13:38:47 +0200

    fix: configure vitest for cypress

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-	test: {
-		coverage: {
-			reporter: ["text", "html"],
-			exclude: ["tests/", "*.config.ts"],
-		},
-	},
-});
+    test: {
+        setupFiles: 'vitest.setup.ts',
+        coverage: {
+            reporter: ['text', 'html'],
+            exclude: ['tests/', '*.config.ts'],
+        },
+        exclude: [
+            '**/node_modules/**', // Default exclusion
+            '**/dist/**', // Default exclusion
+            '**/cypress/**', // Exclude the Cypress directory
+            '**/*.cy.ts', // Exclude Cypress test files by extension if they're in the same directory
+            '**/*.cy.js', // Exclude Cypress test files by extension if they're in the same directory
+        ],
+    },
+})

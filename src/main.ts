005be60cc91ba63f53b79bd3902ab3d634871ad7
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-26 05:57:13 +0200

    feat: add basic API endpoint for users using Oak

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts'
 import { connectToDatabase } from './database/connection.ts'
+import router from './routes.ts'
 
 await connectToDatabase()
 
-console.log('Application is running...')
+const app = new Application()
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+console.log(`Server is running on http://localhost:8000`)
+await app.listen({ port: 8000 })

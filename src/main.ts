ac3a208c6cacbb6e76700ec1afd8be83207f4643
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-26 05:17:53 +0200

    feat: create main application entry point in main.ts

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import config from './config.ts'
+
+console.log('Database User:', config.DB_USER)
+console.log('Database Name:', config.DB_NAME)
+console.log('Database Host:', config.DB_HOST)
+console.log('Database Port:', config.DB_PORT)
+console.log('Database Password:', config.DB_PASSWORD)

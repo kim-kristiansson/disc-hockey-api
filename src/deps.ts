ebf6ef2de23df5a73abb8b2fe878da1d306bda21
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-08-27 20:40:41 +0200

    chore: add dependencies

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+export { Application, Router } from 'https://deno.land/x/oak@v16.1.0/mod.ts'
+export type { RouterContext } from 'https://deno.land/x/oak@v16.1.0/router.ts'
+
+export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts'
+export { config } from 'https://deno.land/x/dotenv@v3.2.2/mod.ts'

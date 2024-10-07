b34f44c6a3a4f181899e92ff46005e99f1d84b2c
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-10-08 01:25:30 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { Application } from './deps.ts'
 import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import playlistRoutes from './routes/playlistRoutes.ts'
+import { sessionMiddleware } from './middleware/session.ts' // Correct path
 
 const app = new Application()
 
+app.use(sessionMiddleware)
 app.use(authRoutes.routes())
 app.use(authRoutes.allowedMethods())
+app.use(userRoutes.routes())
+app.use(userRoutes.allowedMethods())
+app.use(playlistRoutes.routes())
+app.use(playlistRoutes.allowedMethods())
 
 console.log('Server running on http://localhost:3000')
 await app.listen({ port: 3000 })

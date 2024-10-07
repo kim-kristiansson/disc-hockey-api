745ded9ef0755dba3dec1da837e994628e68d558
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-10-08 01:25:41 +0200

    Update spotifyService.ts

--- src/services/spotifyService.ts
+++ src/services/spotifyService.ts
@@ -25,9 +25,33 @@ export const exchangeCodeForToken = async (code: string) => {
     })
 
     if (tokenResponse.ok) {
-        return await tokenResponse.json()
+        const tokenData = await tokenResponse.json()
+        const currentTime = Date.now()
+        tokenData.expiryTime = currentTime + tokenData.expires_in * 1000 // Convert to milliseconds
+        return tokenData
     } else {
         console.error('Error fetching token:', await tokenResponse.json())
+        return null
+    }
+}
+
+export const refreshAccessToken = async (refreshToken: string) => {
+    const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: 'Basic ' + btoa(`${env.SPOTIFY_CLIENT_ID}:${env.SPOTIFY_CLIENT_SECRET}`),
+        },
+        body: new URLSearchParams({
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken,
+        }),
+    })
+
+    if (tokenResponse.ok) {
+        return await tokenResponse.json()
+    } else {
+        console.error('Error refreshing token:', await tokenResponse.json())
         return null
     }
 }

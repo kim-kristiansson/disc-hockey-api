0ad2100e74b6ac804c28f55dcf2697253f41a79a
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-10-08 01:25:28 +0200

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,10 @@
-export { Application, Router } from 'https://deno.land/x/oak@v16.1.0/mod.ts'
+// src/deps.ts
+
+// Value exports
+export { Application, Router, Context } from 'https://deno.land/x/oak@v16.1.0/mod.ts'
+export type { Middleware } from 'https://deno.land/x/oak@v16.1.0/mod.ts'
+
+// Type-only exports
 export type { RouterContext } from 'https://deno.land/x/oak@v16.1.0/router.ts'
 
 export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts'

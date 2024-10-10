e46a4675d15629d9e8ba03155db1cc54a35567ba
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-10-10 23:21:37 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { Application } from './deps.ts'
 import { config } from './deps.ts'
 import router from './routes/index.ts'
+import { closeDatabase, initializeDatabase } from './utils/database.ts'
 
 const app = new Application()
+
+initializeDatabase()
 
 const { PORT } = config()
 
@@ -12,6 +15,11 @@ app.use(router.allowedMethods())
 app.use((ctx) => {
     ctx.response.status = 404
     ctx.response.body = 'Not Found'
+})
+
+addEventListener('beforeExit', () => {
+    console.log('Closing database connection...')
+    closeDatabase()
 })
 
 await app.listen({ port: Number(PORT) || 8000 })

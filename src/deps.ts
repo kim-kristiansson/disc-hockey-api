6a07f447302c71621616d78af8ae987e142a4bb8
Kim Kristiansson <kimkristiansson.dev@protonmail.com>
2024-10-10 23:21:35 +0200

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,4 @@
 export { Application, Router } from 'https://deno.land/x/oak@v17.1.0/mod.ts'
 export { config } from 'https://deno.land/x/dotenv@v3.2.2/mod.ts'
+export { DB } from 'https://deno.land/x/sqlite@v3.9.1/mod.ts'
+export { ensureDirSync } from 'https://deno.land/std@0.224.0/fs/mod.ts'

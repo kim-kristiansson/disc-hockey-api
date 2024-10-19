aaed33f6dfcddde5aa5cbec28b0e7c6ff467fab6
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-19 22:27:59 +0200

    Create IBaseRepository.cs

---
+++ DiscHockey/DiscHockey.Api/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscHockey.Api.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity?> AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        Task<TEntity?> FindByIdAsync(Guid id);
+        Task<IEnumerable<TEntity?>> GetAllAsync();
+        Task<bool> SaveChangesAsync();
+    }
+}

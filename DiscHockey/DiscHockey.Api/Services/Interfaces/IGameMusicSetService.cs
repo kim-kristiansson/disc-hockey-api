f037a1954a5af77c27fbb7c02563091d1f935e75
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-19 17:44:24 +0200

    Create IGameMusicSetService.cs

---
+++ DiscHockey/DiscHockey.Api/Services/Interfaces/IGameMusicSetService.cs
@@ -0,0 +1,10 @@
+﻿using DiscHockey.Api.Dtos;
+using DiscHockey.Api.Models;
+
+namespace DiscHockey.Api.Services.Interfaces
+{
+    public interface IGameMusicSetService
+    {
+        GameMusicSet GetGameMusicSetAsync(RequestDtoGameMusicSet requestDtoGameMusicSet, Guid userId);
+    }
+}

ca69a0742df977d4b94030d7aae77d40a818e631
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-19 17:44:22 +0200

    Create GameMusicSetController.cs

---
+++ DiscHockey/DiscHockey.Api/Controllers/GameMusicSetController.cs
@@ -0,0 +1,26 @@
+﻿using DiscHockey.Api.Dtos;
+using DiscHockey.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SpotifyAPI.Web;
+
+namespace DiscHockey.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameMusicSetController(IGameMusicSetService gameMusicSetService, IUserService userService) :ControllerBase
+    {
+        public async Task<IActionResult> Index([FromBody] RequestDtoGameMusicSet requestDtoGameMusicSet)
+        {
+            if (HttpContext.Items["SpotifyUser"] is not PrivateUser spotifyUser)
+            {
+                return Unauthorized("User is not authorized with Spotify");
+            }
+
+            var user = await userService.FindOrCreateUser(spotifyUser, string.Empty);
+
+            var gameMusicSet = gameMusicSetService.GetGameMusicSetAsync(requestDtoGameMusicSet, user.Id);
+
+            return Ok(gameMusicSet);
+        }
+    }
+}

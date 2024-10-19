3a86009c98155c48c3bca1acc48a928db9002453
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-19 22:27:57 +0200

    Create BaseRepository.cs

---
+++ DiscHockey/DiscHockey.Api/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using DiscHockey.Api.Data;
+using DiscHockey.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscHockey.Api.Repositories
+{
+    public class BaseRepository<TEntity>(AppDbContext context) :IBaseRepository<TEntity> where TEntity : class
+    {
+        protected readonly AppDbContext _context = context;
+
+        public async Task<TEntity?> AddAsync(TEntity entity)
+        {
+            if (entity != null)
+            {
+                await _context.Set<TEntity>().AddAsync(entity);
+
+                return entity;
+            }
+
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Remove(entity);
+            }
+
+            throw new ArgumentNullException(nameof(entity));
+        }
+
+        public async Task<TEntity?> FindByIdAsync(Guid id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity?>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(TEntity entity)
+        {
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Update(entity);
+            }
+
+            throw new ArgumentNullException(nameof(entity));
+        }
+    }
+}

c4ab5e80af132acb42838330871eaec8b2359ece
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-19 22:28:04 +0200

    Update IUserRepository.cs

--- DiscHockey/DiscHockey.Api/Repositories/Interfaces/IUserRepository.cs
+++ DiscHockey/DiscHockey.Api/Repositories/Interfaces/IUserRepository.cs
@@ -4,9 +4,6 @@ namespace DiscHockey.Api.Repositories.Interfaces
 {
     public interface IUserRepository
     {
-        Task AddUserAsync(User user);
         Task<User?> FindUserBySpotifyAsyncId(string spotifyId);
-        Task<bool> SaveChangesAsync();
-        void UpdateUser(User user);
     }
 }

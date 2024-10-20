28c902ca140a1ffd0644fe4ba2c58060f9fa8601
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-20 15:07:13 +0200

    Update GameMusicSetController.cs

--- DiscHockey/DiscHockey.Api/Controllers/GameMusicSetController.cs
+++ DiscHockey/DiscHockey.Api/Controllers/GameMusicSetController.cs
@@ -1,5 +1,6 @@
 ﻿using DiscHockey.Api.Attributes;
 using DiscHockey.Api.Dtos;
+using DiscHockey.Api.Repositories.Interfaces;
 using DiscHockey.Api.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using SpotifyAPI.Web;
@@ -8,20 +9,25 @@ namespace DiscHockey.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class GameMusicSetController(IGameMusicSetService gameMusicSetService, IUserService userService) :ControllerBase
+    public class GameMusicSetController(IGameMusicSetService gameMusicSetService, IUserRepository userRepository) :ControllerBase
     {
         [HttpPost]
         [SpotifyAuthorize]
-        public async Task<IActionResult> Index([FromBody] RequestDtoGameMusicSet requestDtoGameMusicSet)
+        public async Task<IActionResult> CreateGameMusicSet([FromBody] RequestDtoGameMusicSet requestDtoGameMusicSet)
         {
             if (HttpContext.Items["SpotifyUser"] is not PrivateUser spotifyUser)
             {
                 return Unauthorized("User is not authorized with Spotify");
             }
 
-            var user = await userService.FindOrCreateUser(spotifyUser, string.Empty);
+            var user = await userRepository.FindUserBySpotifyIdAsync(spotifyUser.Id);
 
-            var gameMusicSet = gameMusicSetService.GetGameMusicSetAsync(requestDtoGameMusicSet, user.Id);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var gameMusicSet = await gameMusicSetService.CreateGameMusicSetAsync(requestDtoGameMusicSet, user.Id);
 
             return Ok(gameMusicSet);
         }

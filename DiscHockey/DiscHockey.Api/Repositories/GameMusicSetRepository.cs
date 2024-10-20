9fd62bd00fed92f692664b717e339a99b436838b
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-20 15:07:42 +0200

    Create GameMusicSetRepository.cs

---
+++ DiscHockey/DiscHockey.Api/Repositories/GameMusicSetRepository.cs
@@ -0,0 +1,11 @@
+﻿using DiscHockey.Api.Data;
+using DiscHockey.Api.Models;
+using DiscHockey.Api.Repositories.Interfaces;
+
+namespace DiscHockey.Api.Repositories
+{
+    public class GameMusicSetRepository(AppDbContext appDbContext) :BaseRepository<GameMusicSet>(appDbContext), IGameMusicSetRepository
+    {
+
+    }
+}

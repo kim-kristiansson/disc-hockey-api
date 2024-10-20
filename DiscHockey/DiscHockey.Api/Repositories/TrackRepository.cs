0ed253785f8e3d3d818cf90eab5c35e22d888788
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-21 00:22:52 +0200

    Create TrackRepository.cs

---
+++ DiscHockey/DiscHockey.Api/Repositories/TrackRepository.cs
@@ -0,0 +1,10 @@
+﻿using DiscHockey.Api.Data;
+using DiscHockey.Api.Models;
+using DiscHockey.Api.Repositories.Interfaces;
+
+namespace DiscHockey.Api.Repositories
+{
+    public class TrackRepository(AppDbContext appDbContext) :BaseRepository<Track>(appDbContext), ITrackRepository
+    {
+    }
+}

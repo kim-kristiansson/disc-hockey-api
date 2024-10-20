df9a21676a949fbf8a13227f18766e372fda051f
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-20 15:07:24 +0200

    Update RequestDtoPlaylist.cs

--- DiscHockey/DiscHockey.Api/Dtos/Request/RequestDtoPlaylist.cs
+++ DiscHockey/DiscHockey.Api/Dtos/Request/RequestDtoPlaylist.cs
@@ -1,11 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace DiscHockey.Api.Dtos
+﻿namespace DiscHockey.Api.Dtos
 {
     public class RequestDtoPlaylist
     {
-        [Required]
-        public string Name { get; set; } = "";
         public List<RequestDtoTrack> Tracks { get; set; } = [];
     }
 }

40bf6a0692877f32e4c426b7d9b1264c723feb35
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-20 15:07:51 +0200

    Create PlaylistRepository.cs

---
+++ DiscHockey/DiscHockey.Api/Repositories/PlaylistRepository.cs
@@ -0,0 +1,10 @@
+﻿using DiscHockey.Api.Data;
+using DiscHockey.Api.Models;
+using DiscHockey.Api.Repositories.Interfaces;
+
+namespace DiscHockey.Api.Repositories
+{
+    public class PlaylistRepository(AppDbContext appDbContext) :BaseRepository<Playlist>(appDbContext), IPlaylistRepository
+    {
+    }
+}

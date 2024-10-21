ddf5e762ca9d01119171d52ca18bdbf0d510273f
Kim Kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-10-21 19:21:42 +0200

    Create UserController.cs

---
+++ DiscHockey/DiscHockey.Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DiscHockey.Api.Attributes;
+using DiscHockey.Api.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SpotifyAPI.Web;
+
+namespace DiscHockey.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController(IUserRepository userRepository) :ControllerBase
+    {
+        [HttpGet("me")]
+        [SpotifyAuthorize]
+        public async Task<IActionResult> GetUser()
+        {
+            if (HttpContext.Items["SpotifyUser"] is not PrivateUser spotifyUser)
+            {
+                return Unauthorized("User is not authorized with Spotify");
+            }
+
+            var user = await userRepository.FindUserBySpotifyIdAsync(spotifyUser.Id);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
+    }
+}
